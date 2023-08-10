83e29711cd1a7ef33deffe66ac278818c3320489
angel <angelo.viana@hotmail.com>
2023-08-09 21:12:54 -0300

    Adicionar arquivos de projeto.

---
+++ exercício_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        double numero, valorHora, horaTrabalhada, salario;
+       
+
+        Console.WriteLine("\nDigite o número do funcionário: ");
+        numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine("\nDigite as horas trabalhadas: ");
+        horaTrabalhada = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine("\nDigite o valor do valor hora: ");
+        valorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        salario = valorHora * horaTrabalhada;
+
+        Console.WriteLine("\nNumber:  " + numero, CultureInfo.InvariantCulture);
+        Console.WriteLine("Salary U$:  " + salario.ToString("F2", CultureInfo.InvariantCulture));
+    }
+}
